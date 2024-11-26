723c05cd0d67b5add6f1a9b5ab8a4152a64fa1a0
BraisPN <braispose15@gmail.com>
2024-11-26 11:37:28 +0100

    Update exer4.py

--- Python/t02_08_Pose_Nieto/exer4.py
+++ Python/t02_08_Pose_Nieto/exer4.py
@@ -38,31 +38,78 @@ __author__ = "Brais Pose Nieto"
 
 lista = []
 
-def ingresar_datos(nome, apelidos, nota, lista, alumno):
+def ingresar_datos(nome:str, apelidos:str, nota:float, lista:list):
     if not type(nome) == str:
         raise ValueError
     elif not type(apelidos) == str:
         raise ValueError
     elif not type(nota) == float:
         raise ValueError
+    if nota < 0 or nota > 10:
+        raise ValueError
     alumno = {"nome" : nome, "apelidos" : apelidos, "nota" : nota}
     lista.append(alumno)
 
+def eliminar_alumno(eliminar:str ,lista:list):
+    if not type(eliminar) == str:
+        raise ValueError
+    for i, alumno in enumerate(lista):
+        if alumno["nome"] == eliminar:
+            del lista[i]
 
+def modificar_datos(modificar:str, lista:list, nova_nota:float):
+    if not type(modificar) == str:
+        raise ValueError
+    if not type(nova_nota) == float:
+        raise ValueError
+    for alumno in lista:
+        if alumno["nome"] == modificar:
+            alumno["nota"] = nova_nota
 
+def ver_aprobados(lista:list):
+    aprobados = []
+    for alumno in lista:
+        if alumno["nota"] >= 5:
+            aprobado = alumno["nome"] + " " + alumno["apelidos"]
+            aprobados.append(aprobado)
+    
+    return aprobados
 
+while True:
+    print("a) Ingresar datos alumno")
+    print("b) Eliminar datos alumno")
+    print("c) Modificar nota alumno")
+    print("d) Ver nomes e apelidos de alumnos aprobados")
+    print("g) Sair")
+    opcion = input("-> ")
+    if opcion == "a":
+        nome = str(input("Nome: "))
+        apelidos = str(input("Apelidos: "))
+        nota = float(input("Nota: "))
+        ingresar_datos(nome, apelidos, nota, lista)
 
+    elif opcion == "b":
+        print("Alumnos:")
+        for i, alumno in enumerate(lista):
+            print(alumno["nome"], alumno["apelidos"], alumno["nota"])
+        eliminar = str(input("Alumno a eliminar: "))
+        eliminar_alumno(eliminar, lista)
+    
+    elif opcion == "c":
+        print("Alumnos:")
+        for i, alumno in enumerate(lista):
+            print(alumno["nome"], alumno["apelidos"], alumno["nota"])
+        modificar = str(input("Alumno a modificar: "))
+        nova_nota = float(input("Nova nota: "))
+        modificar_datos(modificar, lista, nova_nota)
 
+    elif opcion == "d":
+        aprobados = ver_aprobados(lista)
+        for aprobado in aprobados:
+            print(aprobado)
 
-print("a) Ingresar datos alumno")
-opcion = input("-> ")
-
-if opcion == "a":
-    nome = str(input("Nome: "))
-    apelidos = str(input("Apelidos: "))
-    nota = float(input("Nota: "))
-
-
-
+    elif opcion == "g":
+        print("Saindo do programa...")
+        break
 
 
